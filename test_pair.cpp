221e161790b7e7fa22bd0b6abc8fa93e9be7a687
Griborii <grao319@yandex.ru>
2021-03-15 15:45:39 +0300

    па
    п

--- test_pair.cpp
+++ test_pair.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
@@ -9,10 +10,67 @@ class Pair {
 public:
     T1 first;
     T2 second;
+    Pair () {
+    	
+	}
+    Pair (const Pair& copied) {
+    	first = copied.first;
+    	second = copied.second;
+	}
+    Pair& operator=(const Pair& copied) {
+    	(*this).first = copied.first;
+    	(*this).second = copied.second;
+	}
+	bool operator==(const Pair& p1) const{
+		if ((*this).first == p1.first && (*this).second == p1.second) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator!=(const Pair& p1) {
+		return ((p1 == (*this)) ^ true);
+	}
+	bool operator<(const Pair& p1) const{
+		if ((*this).first < p1.first) {
+			return true;
+		}
+		if ((*this).first > p1.first) {
+			return false;
+		}
+		if ((*this).first < p1.second) {
+			return true;
+		}
+		if ((*this).first > p1.second) {
+			return false;
+		}
+	}
+	bool operator>(const Pair& p1) const{
+		return (p1 < (*this));
+	}
+	bool operator<=(const Pair& p1) const{
+		return (((*this) < p1) | ((*this) == p1));
+	}
+	bool operator>=(const Pair& p1) const{
+		return (((*this) > p1) | ((*this) == p1));
+	}
+	void swap (Pair p1, Pair p2) {
+		Pair p3 = p2;
+		p2 = p1;
+		p1 = p3;
+	}
 };
 
 template<typename T>
 void mySort(vector<T>& a) {
+	for (int i = 0; i < a.size(); ++i) {
+		for (int j = 0; j < i; ++j) {
+			if (a[i] < a[j]) {
+				swap(a[i], a[j]);
+			}
+		}
+	}
 }
 
 int main() {
